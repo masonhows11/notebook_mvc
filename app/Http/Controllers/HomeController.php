8851076412156a675d8040e700cad411168ae9c9
mason.hows11 <mason.hows11@gmail.com>
2024-01-22 22:52:29 +0330

    project template updated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,13 +4,21 @@
 namespace App\Http\Controllers;
 
 
-use App\Models\User;
+use App\Models\Contact;
+
 
 class HomeController extends Controller
 {
+    private $contactModel;
+
+    public function __construct()
+    {
+        $this->contactModel = new Contact();
+    }
 
     public function index()
     {
+        $allContact = $this->contactModel->getAll();
         return view('home');
     }
 
