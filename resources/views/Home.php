179720892c42be3e524676eebd082a3b6e8f230c
mason.hows11 <mason.hows11@gmail.com>
2024-01-23 21:22:52 +0330

    add form for add user

--- resources/views/Home.php
+++ resources/views/Home.php
@@ -11,14 +11,14 @@
 <body>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">پردازش همکاران پارسه</a>
+        <a class="navbar-brand" href="/">پردازش همکاران پارسه</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link " href="#">خانه</a>
+                    <a class="nav-link " href="/">خانه</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">جدید</a>
@@ -31,21 +31,28 @@
 <div class="container">
     <div class="row mt-5">
         <div class="col-sm-6">
-            <form>
+            <form action="#" method="post">
                 <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    <label for="name" class="form-label">نام کاربری</label>
+                    <input type="text" name="name" id="name" class="form-control">
                 </div>
                 <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1">
+                    <label for="first_name" class="form-label">نام</label>
+                    <input type="text" name="first_name" id="first_name" class="form-control">
                 </div>
-                <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                <div class="mb-3">
+                    <label for="email" class="form-label">ایمیل</label>
+                    <input type="email" class="form-control" id="email">
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <div class="mb-3">
+                    <label for="mobile" class="form-label">موبایل</label>
+                    <input type="text" class="form-control" id="mobile">
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">رمز عبور</label>
+                    <input type="password" class="form-control" id="password">
+                </div>
+                <button type="submit" class="btn btn-primary">ذخیره</button>
             </form>
         </div>
         <div class="col-sm-6">
