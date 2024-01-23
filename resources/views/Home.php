682826afaf1d7edf896cb6b9ec93281436887e6d
mason.hows11 <mason.hows11@gmail.com>
2024-01-23 17:00:38 +0330

    read data in foreach broblem

--- resources/views/Home.php
+++ resources/views/Home.php
@@ -28,6 +28,54 @@
     </div>
 </nav>
 
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-sm-6">
+            <form>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                </div>
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+        <div class="col-sm-6">
+            <table class="table">
+                <thead>
+                <tr>
+                    <th >#</th>
+                    <th >نام</th>
+                    <th >نام  خانوادگی</th>
+                    <th >ایمیل</th>
+                    <th>موبایل</th>
+                    <th>عملیات</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?= foreach ($contacts as $contact): ?>
+                <tr>
+                    <th><?= $contact['id'] ?></th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td><a href="#">حذف</a> <a href="#">ویرایش</a></td>
+                </tr>
+                <?= endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
 
     <?php include BASE_PATH.'resources/views/include/scripts.php';  ?>
 </body>
