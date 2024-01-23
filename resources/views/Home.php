f408137b0884f8c79640009360178709abdc4cf7
mason.hows11 <mason.hows11@gmail.com>
2024-01-23 16:50:45 +0330

    add fonts

--- resources/views/Home.php
+++ resources/views/Home.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="fa" dir="rtl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
@@ -8,11 +8,26 @@
     <title>Home</title>
     <?php include  BASE_PATH.'resources/views/include/styles.php';  ?>
 </head>
-<body style="background-color: aliceblue">
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">پردازش همکاران پارسه</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link " href="#">خانه</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">جدید</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
 
-<div>
-    <h2 style="text-align: center">Home Page</h2>
-</div>
 
     <?php include BASE_PATH.'resources/views/include/scripts.php';  ?>
 </body>
