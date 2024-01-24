b069dfbab7bad9e0c3e2c40856dacb901728965a
mason.hows11 <mason.hows11@gmail.com>
2024-01-24 17:47:45 +0330

    search on multi column fixed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
         $search_keyword = $request->inputKey('search');
         if (!is_null($request->inputKey('search'))) {
             // var_dump($request->inputKey('search'));
-            $where["name[~]"] = $request->inputKey('search');
+            // $where["name[~]"] = $request->inputKey('search');
 
             // in this condition we store condition query in $where array variable
             //            $where['AND'] = [
@@ -37,12 +37,12 @@ class HomeController extends Controller
             //                ]
             //            ];
 
-            //            $where['OR'] = [
-            //                "name[~]" => $search_keyword,
-            //                "user_name[~]" => $search_keyword,
-            //                "email[~]" => $search_keyword,
-            //                "mobile[~]" => $search_keyword,
-            //            ];
+            $where['OR'] = [
+                "name[~]" => $search_keyword,
+                "first_name[~]" => $search_keyword,
+                "email[~]" => $search_keyword,
+                "mobile[~]" => $search_keyword,
+            ];
             // $allContact = $this->contactModel->get('*', $where);
             //var_dump($allContact);
         }
